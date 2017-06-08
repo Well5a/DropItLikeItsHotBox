4d85ecbaa1d4f3677add158b4bc296cd31af39ea
David Schick <david.schick@online.de>
2017-06-08 16:49:46 +0200

    Authorization & Authentication Filter added

--- Dropbox/src/main/java/com/dropbox/dao/FilepermissionDao.java
+++ Dropbox/src/main/java/com/dropbox/dao/FilepermissionDao.java
@@ -1,6 +1,9 @@
 package com.dropbox.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import com.dropbox.dao.DaoManager;
 
 import model.Filepermission;
@@ -25,6 +28,15 @@ public class FilepermissionDao {
 		return em.find(Filepermission.class, id);
 	}
 	
+	public Filepermission getPermissionsOfUserForFile(int fileid, int uid)
+	{
+		Query q = em.createQuery("SELECT * FROM dropbox.filepermission WHERE fileId=:fileid AND userId=:userid ");
+		q.setParameter("fileid", fileid);
+		q.setParameter("userid", uid);
+		
+		return (Filepermission) q.getResultList().get(0);
+	}
+		
 	public void insertFilepermission(Filepermission fp)
 	{
 		em.getTransaction().begin();
